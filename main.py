09ecac21d3563a47ca890fd2398208e25f3a6e13
danilpiatov <71121112+danilpiatov@users.noreply.github.com>
2021-06-23 20:25:27 +0300

    Add free speech recognition and neural network

--- main.py
+++ main.py
@@ -1,43 +1,131 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, MaxPooling1D, Conv1D, GlobalMaxPooling1D, Dropout, LSTM, GRU
+from tensorflow.keras import utils
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import utils
+import pandas as pd
+import numpy as np
+import random
 import time
 import telebot
 import requests
-import json
+import subprocess
 import pyttsx3
+import json
+import time
+import telebot
+import requests
+import subprocess
+import pyttsx3
+import json
+
+API_ENDPOINT = 'https://api.wit.ai/speech'
+
+# Wit.ai api access token
+wit_access_token = 'ZIJAR4WEPUZEVJHYEVZYUXLZZNZXKHQO'
 
 
 TG_API = '1889784597:AAH9muA6PWCt4eGxEVeJn9KTSLRi_EVkJpQ'
 bot = telebot.TeleBot(TG_API)
 
-URL = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
-IAM = "t1.9euelZqRk5HOmI7GjZDHkc6Kzs_Iyu3rnpWazpjIkZLHyIyYi8fJnpKRjcnl9PccFVZ5-e8FYjSI3fT3XENTefnvBWI0iA.A-sWNkjVU2jkZ1Jgazu1ZsbmAuxvNmHPVWF62TigfQ6ayFtd4QpKTmGscmtq_nzoArDSY6-g-Qup-CHKhXSuBA"
-ID = "b1g628fpciqlrvm7g00i"
-YA_API = "AQVN1bXy8uunsrBMjQDdSUClHIuT4EmWakiqqAQS"
-
-
 tts = pyttsx3.init()
 RU_VOICE_ID = r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0"
 tts.setProperty('voice', RU_VOICE_ID)
 
-def recognize(voice, IAM, ID):
-    headers = {'Authorization': f'Api-Key {YA_API}'}
 
-    # остальные параметры:
-    params = {
-        'lang': 'ru-RU',
-        'folderId': ID,
-        'sampleRateHertz': 48000,
-    }
+command_classes = 4
 
-    response = requests.post(URL, params=params, headers=headers, data=voice)
+with open("data.txt", "r") as f:
+    data = f.read().split('\n')
 
-    # бинарные ответ доступен через response.content, декодируем его:
-    decode_resp = response.content.decode('UTF-8')
+random.shuffle(data)
 
-    # и загрузим в json, чтобы получить текст из аудио:
+print(data)
 
-    text = json.loads(decode_resp)
 
-    return text['result']
+train_data = "\n"
+test_data = "\n"
+
+with open("train.csv", "w") as f:
+    f.write(train_data.join(data[:1000]))
+
+
+with open("test.csv", "w") as f:
+    f.write(test_data.join(data[1000:]))
+
+
+train = pd.read_csv('data.txt',
+                    header=None,
+                    names=['class', 'text'])
+#
+commands = train['text']
+#
+y_train = utils.to_categorical(train['class'] - 1, command_classes)
+#
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(commands)
+seqs = tokenizer.texts_to_sequences(commands)
+
+x_train = pad_sequences(seqs, maxlen=10)
+
+model_lstm = Sequential()
+model_lstm.add(Embedding(10000, 64, input_length=10))
+model_lstm.add(LSTM(32))
+model_lstm.add(Dense(4, activation='softmax'))
+
+model_lstm.compile(optimizer='adam',
+               loss='categorical_crossentropy',
+               metrics=['accuracy'])
+
+model_lstm_save_path = 'best_model_lstm.h5'
+checkpoint_callback_lstm = ModelCheckpoint(model_lstm_save_path,
+                                       monitor='val_accuracy',
+                                       save_best_only=True,
+                                       verbose=1)
+
+history_lstm = model_lstm.fit(x_train,
+                            y_train,
+                            epochs=5,
+                            batch_size=128,
+                            validation_split=0.1,
+                             callbacks=[checkpoint_callback_lstm])
+
+test = pd.read_csv('test.csv',
+                    header=None,
+                    names=['class', 'text'])
+
+test_sequences = tokenizer.texts_to_sequences(test['text'])
+x_test = pad_sequences(test_sequences, maxlen=10)
+
+y_test = utils.to_categorical(test['class'] - 1, 4)
+
+model_lstm.load_weights(model_lstm_save_path)
+res = model_lstm.evaluate(x_test, y_test, verbose=1)
+print(res)
+
+
+
+
+def RecognizeSpeech(audio):
+
+    # defining headers for HTTP request
+    headers = {'authorization': 'Bearer ' + wit_access_token,
+               'Content-Type': 'audio/wav'}
+
+    # making an HTTP post request
+    resp = requests.post(API_ENDPOINT, headers=headers,
+                         data=audio)
+
+    # converting response content to JSON format
+    data = json.loads(resp.content)
+
+    # get text from data
+    text = data['text']
+
+    # return the text
+    return text
 
 
 @bot.message_handler(content_types=['voice'])
@@ -47,12 +135,31 @@ def voice_processing(message):
     with open('voice.ogg', 'wb') as dfile:
         dfile.write(file)
 
-    with open('voice.ogg', 'rb') as f:
+    sourceFile = 'voice.ogg'
+    resultFile = 'voice.wav'
+    options = '--force-wav'
+    cmd = 'opusdec.exe ' + options + ' ' + sourceFile + ' ' + resultFile
+    subprocess.run(cmd)
+    time.sleep(0.3)
+
+    with open('voice.wav', 'rb') as f:
         voice = f.read()
 
-    text = recognize(voice, IAM, ID)
+    text = RecognizeSpeech(voice)
     print(text)
-    tts.save_to_file(text, 'answer.ogg')
+
+    command = text
+    sequence = tokenizer.texts_to_sequences([command])
+    data = pad_sequences(sequence, maxlen=10)
+    result = model_lstm.predict(data)
+    print(result)
+
+    i = np.argmax(result)
+
+    commands_dict = {0: 'записываю на занятие', 1: 'говорю подробнее',
+                     2: 'показываю услуги', 3: 'проверяю твой уровень'}
+
+    tts.save_to_file(commands_dict.get(i, 'не понял'), 'answer.ogg')
     tts.runAndWait()
     time.sleep(0.3)
     answer = open('answer.ogg', 'rb')
@@ -60,3 +167,5 @@ def voice_processing(message):
 
 
 bot.polling()
+
+
